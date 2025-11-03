e5f4f66a38b89b6b62d30da350c4adb648d5fa67
Carl Mes <carl.mes@gmail.com>
2025-11-03 14:17:57 -0700

    Added a project bootstrap.sh for installing with one-click

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+apply_firmly(){
+  if [ ! -f "${1}/kustomization.yaml" ]; then
+    print_error "Please provide a dir with \"kustomization.yaml\""
+    return 1
+  fi
+
+  # kludge
+  until oc kustomize "${1}" --enable-helm | oc apply -f- 2>/dev/null
+  do
+    echo -n "."
+    sleep 5
+  done
+  echo ""
+  # until_true oc apply -k "${1}" 2>/dev/null
+}
+
+
+echo "Creating the city-permitting-agent-ui namespace, which holds the custom UI"
+oc new-project city-permitting-agent-ui
+
+echo "Creating the llama-serve namespace, which holds Llama Stack"
+oc new-project llama-serve
+
+echo "At this point, please import the cluster secrets following the installation guide"
+pause
+
+echo "Installing the City Permitting Agent runtime components"
+apply_firmly kubernetes/kustomize/overlay/maas
+
+echo "Done"
